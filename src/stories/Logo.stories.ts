ac03a84bf488abcacce92b533cb48b85a1008833
Bilal-Ghiffari <bilalalghiffari53@gmail.com>
2023-09-13 21:01:44 +0700

    clear boilerplate + create logo component

---
+++ src/stories/Logo.stories.ts
@@ -0,0 +1,14 @@
+import "./assets/index.css";
+import type { Meta, StoryObj } from "@storybook/react";
+import { Logo } from "../components/Logo";
+
+const meta: Meta = {
+  title: "Components/Logo",
+  component: Logo,
+  tags: ["autodocs"],
+};
+export default meta;
+type Story = StoryObj;
+export const Primary: Story = {
+  args: {},
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = {
+  primary: "#51459E",
+  secondary: "#9698AB",
+};
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ...colors,
+      },
+      fill: {
+        ...colors,
+      },
+    },
   },
   plugins: [],
 };
